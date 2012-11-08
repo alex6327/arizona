ba6681188bac69744f0a27b36e15a16d7787bc7f
Alex_2 <Alex_2@192.168.0.82>
2012-11-07 16:32:31 -0800

    Nov. 7th

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -17,10 +17,15 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
-	}
+    function __construct() {
+        parent::__construct();
+    }
+    public function index()
+    {
+            $this->load->library('session');
+            $this->load->database();
+            $this->load->view('template');
+    }
 }
 
 /* End of file welcome.php */
---
+++ application/views/_meta.php
@@ -0,0 +1,32 @@
+<?php
+if(!defined('APPPATH'))
+    exit('No direct script access allowed');
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>示范点</title>
+    <link rel="icon" type="image/png" href="imgs/logo.png" />
+    <link type="text/css" href="css/flick/jquery-ui-1.8.17.custom.css" rel="stylesheet" />	 
+    <script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
+    <script type="text/javascript" src="js/jquery-ui-1.8.17.custom.min.js"></script>
+    <script type="text/javascript">
+        $(function(){
+            // Tabs
+            $('#tabs').tabs();
+        });
+    </script>
+
+    <style type="text/css">
+    div {background-color:#FFF;}
+    a:link, a:visited {
+            color:#00F;
+            text-decoration:underline;
+    }
+
+    </style>
+</head>
